96936383b66d9de8331fa8ab768bc03a6fc2484c
MirijaMarc <mirijarazafimbelo30@gmail.com>
2023-11-21 11:24:50 +0300

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,6 @@ function App() {
 
   return (
     <>
-      {/* <header>
-          <nav className="navbar bg-body-tertiary" style="background-color: #ededed40;">
-                  <div className="container-fluid">
-                  <a className="navbar-brand" href="./etat">
-                      <div className="d-flex align-items-center">
-                          <img src="./assets/img/stock.svg" alt="Logo" width="50" height="44" className="d-inline-block align-text-top" />
-                              &nbsp;NBA
-                      </div>
-                  </a>
-                  
-                  </div>
-          </nav>
-      </header> */}
       <div className='container mt-3'>
         <div className="row">
           <ListeJoueurs />
