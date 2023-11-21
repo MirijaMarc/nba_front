b77268db56c40d6654507df169f086962a6344c1
MirijaMarc <mirijarazafimbelo30@gmail.com>
2023-11-21 11:53:28 +0300

    Liste joueurs

--- src/components/ListeJoueurs.jsx
+++ src/components/ListeJoueurs.jsx
@@ -6,56 +6,72 @@ function ListeJoueurs(){
         <table className="table table-striped">
             <thead>
             <tr>
-                <th scope="col">Numero</th>
-                <th scope="col">Article</th>
-                <th scope="col">Unité</th>
-                <th scope="col">Quantité initiale</th>
-                <th scope="col">Quantité restante</th>
-                <th scope="col">Prix Unitaire</th>
-                <th scope="col">Montant</th> 
-                <th scope="col">Magasin</th> 
+                <th scope="col">Classement</th>
+                <th scope="col">Joueur</th>
+                <th scope="col">Equipe</th>
+                <th scope="col">Matchs Joués</th>
+                <th scope="col">Minutes Jouées</th>
+                <th scope="col">Points Par Match</th>
+                <th scope="col">3 points</th>
+                <th scope="col">Rebonds</th> 
+                <th scope="col">Passes Décisives</th> 
             </tr>
             </thead>
             <tbody>
             <tr>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td> 
-                <td>Unité</td> 
-            </tr>
+                <td>1</td>
+                <td>Setphen Curry</td>
+                <td>Golden State</td>
+                <td>15</td>
+                <td>765</td>
+                <td>21</td>
+                <td>41%</td>
+                <td>11</td> 
+                <td>5</td> 
+            </tr> 
             <tr>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td> 
-            </tr>
+                <td>1</td>
+                <td>Setphen Curry</td>
+                <td>Golden State</td>
+                <td>15</td>
+                <td>765</td>
+                <td>21</td>
+                <td>41%</td>
+                <td>11</td> 
+                <td>5</td> 
+            </tr> 
             <tr>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td> 
-            </tr>
+                <td>1</td>
+                <td>Setphen Curry</td>
+                <td>Golden State</td>
+                <td>15</td>
+                <td>765</td>
+                <td>21</td>
+                <td>41%</td>
+                <td>11</td> 
+                <td>5</td> 
+            </tr> 
             <tr>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td>
-                <td>Unité</td> 
+                <td>1</td>
+                <td>Setphen Curry</td>
+                <td>Golden State</td>
+                <td>15</td>
+                <td>765</td>
+                <td>21</td>
+                <td>41%</td>
+                <td>11</td> 
+                <td>5</td> 
+            </tr> 
+            <tr>
+                <td>1</td>
+                <td>Setphen Curry</td>
+                <td>Golden State</td>
+                <td>15</td>
+                <td>765</td>
+                <td>21</td>
+                <td>41%</td>
+                <td>11</td> 
+                <td>5</td> 
             </tr> 
             </tbody>
         </table>
